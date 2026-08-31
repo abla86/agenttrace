b627988241310e0a68b694665e67329a6a5e0daf
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-09-01 00:11:44 +0200

    feat(simulation): export swarm world and threat scenario APIs

--- agenttrace/simulation/__init__.py
+++ agenttrace/simulation/__init__.py
@@ -6,13 +6,31 @@ from .policy import SimulationPolicy
 from .proposal_runtime import ProposalDecision, ProposalRuntime
 from .proposals import EnrichedProposal, ProposalEngine
 from .state import SimulationState, build_simulation_state
+from .swarm import SwarmEngine
+from .swarm_entities import (
+    AutonomyEvolutionEngine,
+    Civilization,
+    CivilizationEngine,
+    Colony,
+    ColonyEngine,
+    Territory,
+    TerritoryEngine,
+    SwarmWorldEngine,
+)
+from .threats import SceneManager, SceneProfile, ThreatPattern, ThreatProfile, ThreatSimulationEngine, ThreatSpecies
 
 __all__ = [
     "ArenaEngine",
     "AutonomyEngine",
+    "AutonomyEvolutionEngine",
+    "Civilization",
+    "CivilizationEngine",
+    "Colony",
+    "ColonyEngine",
     "ControllerStep",
     "DefenseEngine",
     "DefenseProposal",
+    "DefenseEngine",
     "DriftEngine",
     "DriftState",
     "EnrichedProposal",
@@ -21,9 +39,19 @@ __all__ = [
     "ProposalDecision",
     "ProposalEngine",
     "ProposalRuntime",
+    "SceneManager",
+    "SceneProfile",
     "SimulationController",
     "SimulationPolicy",
     "SimulationState",
+    "SwarmEngine",
+    "SwarmWorldEngine",
+    "Territory",
+    "TerritoryEngine",
+    "ThreatPattern",
+    "ThreatProfile",
+    "ThreatSimulationEngine",
+    "ThreatSpecies",
     "WormEngine",
     "build_simulation_state",
 ]
