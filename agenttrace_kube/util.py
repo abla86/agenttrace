0af7d3eddc55c06263f090ec1fdf27a411040bd1
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-09-01 05:58:27 +0200

    Fix agenttrace_kube/util.py against core API

--- agenttrace_kube/util.py
+++ agenttrace_kube/util.py
@@ -1,9 +1,9 @@
-from agenttrace.api import TraceNode, TaintLabel, sha256
+from agenttrace.api import TaintLabel, TraceNode
 
 
-def make_node(node_id: str, content: str, taint: TaintLabel = TaintLabel.TOOL_OUTPUT_UNTRUSTED) -> TraceNode:
+def make_node(
+    node_id: str,
+    content: str,
+    taint: TaintLabel = TaintLabel.TOOL_OUTPUT_UNTRUSTED,
+) -> TraceNode:
     return TraceNode(node_id, taint, content, ())
-
-
-def hash_content(content: str) -> str:
-    return sha256(content.encode("utf-8"))
