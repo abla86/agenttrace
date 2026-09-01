fda912845901f46b0931e43fdfa7bd1c8a936faf
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-09-01 05:55:42 +0200

    Add verified extension agenttrace_azure/__init__.py

---
+++ agenttrace_azure/__init__.py
@@ -0,0 +1,6 @@
+"""Azure extension for AgentTrace."""
+
+from .monitor import azure_monitor_probe
+from .resource import azure_resource_probe
+
+__all__ = ["azure_monitor_probe", "azure_resource_probe"]
