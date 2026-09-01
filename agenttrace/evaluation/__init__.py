7e017e25f37a9c6e9afc29d5a59f18b01d1d7394
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-09-01 05:33:59 +0200

    refactor: clean evaluation public exports

--- agenttrace/evaluation/__init__.py
+++ agenttrace/evaluation/__init__.py
@@ -1,1 +1,23 @@
-from .models import *
+"""Evaluation models and services."""
+
+from .models import (
+    ActionCapability,
+    AgentPhase,
+    Decision,
+    EvaluationResult,
+    PolicyDecision,
+    TaintLabel,
+    ToolManifest,
+    TraceNode,
+)
+
+__all__ = [
+    "ActionCapability",
+    "AgentPhase",
+    "Decision",
+    "EvaluationResult",
+    "PolicyDecision",
+    "TaintLabel",
+    "ToolManifest",
+    "TraceNode",
+]
